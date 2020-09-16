8af72885ad0bcbb742631b0127d8be02d6511bc8
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-09-16 16:36:47 -0700

    Saving progress. Basic form completed.

--- yessir/src/App.js
+++ yessir/src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form'
 
+//////////////// INITIAL STATES ////////////////
+//////////////// INITIAL STATES ////////////////
+const initalFormValues = {
+  fname: '',        //text input
+  email: '',        //text input
+  password: '',     //text input
+  tosCheckbox: false, //checkbox
+
+}
+
 function App() {
+  const [formValues, setFormValues] = useState(initalFormValues)  //state to hold form values
+  
+
+  //////////////// EVENT HANDLERS ////////////////
+const inputChange = (name, value) => {
+  setFormValues({ ...formValues, [name]: value})
+}
+
   return (
     <div className="App">
       <div className='header'>
         <h1>New User Sign-up</h1>
-        </div>
-        <div className='container'>
-          {/* <Form /> */}
+        <Form  
+        values={formValues}
+        inputChange={inputChange}
+        />
         </div>
     </div>
   );
--- yessir/src/components/Form.js
+++ yessir/src/components/Form.js
@@ -1,9 +1,64 @@
 import React from 'react'
 
 export default function Form(props) {
+    const { values, inputChange } = props
 
+    const onChange = event => {
+        const { name, value, type, checked } = event.target
+        
+        const valueToUse = type === 'checkbox'? checked : value;  
+        inputChange(name, valueToUse)
+    }
+    return (
+        <form className='formContainer'>
 
-    return(
-        console.log('//something')
+            <label htmlFor='fnameInput'>Full Name</label>
+            <input
+                id='fnameInput'
+                name='fname'
+                type='text'
+                placeholder='enter full name'
+                value={values.fname}
+                onChange={onChange}
+            />
+            <br />
+
+            <label htmlFor='emailInput'>Email</label>
+            <input
+                id='emailInput'
+                name='email'
+                type='email'
+                placeholder='enter email address'
+                value={values.email}
+                onChange={onChange}
+            />
+            <br />
+
+            <label htmlFor='pwordInput'>Create Password</label>
+            <input
+                id='pwordInput'
+                name='password'
+                type='password'
+                placeholder='create password'
+                value={values.password}
+                onChange={onChange}
+            />
+            <br />
+
+            <label htmlFor='tosCheckbox'>Check box to agree to Terms of Service</label>
+            <input
+                id='tosCheckbox'
+                name='tosCheckbox'
+                type='checkbox'
+                checked={values.tosCheckbox}
+                onChange={onChange}
+            />
+            <br />
+
+            <div className='submit'>
+                <button>submit</button>
+            </div>
+
+        </form>
     )
 }
